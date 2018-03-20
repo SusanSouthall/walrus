d26116f82c7790240066693723d3709f95374347
Mike Chu and Susan Southall <susan@>
2018-03-20 09:55:41 -0700

    Add light and dark themes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $(".visible").toggle();
-    $(".invisible").toggle();
+    $(".visible").slideToggle();
+    $(".invisible").slideToggle();
+  });
+
+  $("span.light").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light-theme");
+  });
+
+  $("span.dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-theme");
   });
 });
